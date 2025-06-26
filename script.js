6476f0fa71ec44e972a59c21931ba7a4bffae403
EudisonJr <eudisonjunior@acad.ifma.edu.br>
2025-06-25 21:54:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+const titulo = document.querySelector('.sobre .txt-sobre h2');
+typeWrite(titulo);
+
+
